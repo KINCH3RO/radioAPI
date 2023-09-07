db2958eb918e5ab8c0d95e30ca72a1ec5fb591dc
Hatim Rachid <hat83203@gmail.com>
2023-09-07 13:28:01 +0100

    adding radio  functionallities

--- main.ts
+++ main.ts
@@ -1,12 +1,29 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import fs from 'fs'
+import Throttle from "throttle";
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
+const fileStream = fs.createReadStream(process.cwd() + "/assets/songs/Fk.mp3");
+const writableStreams: Response[] = [];
+
+fileStream.pipe(new Throttle(320000 / 8)).on('data', (chunk: Buffer) => {
+	for (const writableStream of writableStreams) {
+		writableStream.write(chunk);
+	}
+})
+
 app.get("/", (req: Request, res: Response) => {
 	res.end("Hello word \n");
+})
+
+app.get("/api/audioStream", (req: Request, res: Response) => {
+
+	res.setHeader("Content-Type","audio/mpeg");
+	writableStreams.push(res);
 })
 
 app.listen(PORT, () => {
